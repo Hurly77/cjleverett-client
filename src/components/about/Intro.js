28e9c1f5aa2084bdff22c68eb5a4d8ca5b517410
cameron <cameron.leverett@yahoo.com>
2020-12-08 13:01:16 -0800

    added intro comp

---
+++ src/components/about/Intro.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const Intro = () => {
+  return (
+   <p>
+     blah blah this is blah I have blah help blah my blah lalal hehe my do se e my ne de
+   </p>
+  )
+}
