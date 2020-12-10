5a83e636f570038cf9b3c37b77f9ef19f8c236ee
cameron <cameron.leverett@yahoo.com>
2020-12-09 22:26:51 -0800

    changed som stuff around

--- src/containers/SiteBar.js
+++ src/containers/SiteBar.js
@@ -1,27 +1,25 @@
-import React from 'react'
+import React, { Component } from 'react'
 import AppBar from '@material-ui/core/AppBar'
 import Toolbar from '@material-ui/core/Toolbar'
 import {makeStyles} from '@material-ui/core/styles'
 import Logo from '../components/Logo'
 import Menu from '../components/Menu'
 
-const useStyle = makeStyles({
-  root: {
-    background: '#0b4f6c',
-  },
-})
+export default class SiteBar extends Component {  
+  render() {
+    const classes = makeStyles({
+      root: {
+        background: '#0b4f6c',
+      },
+    })
 
-
-const SiteBar = () => {
-  const classes = useStyle()
-  return (
-    <AppBar position="absolute" className={classes.root}>
-    <Toolbar>
-      <Logo />
-      <Menu />
-    </Toolbar>
-  </AppBar>
-  )
+    return (
+      <AppBar position="fixed" className={classes.root}>
+      <Toolbar>
+        <Logo />
+        <Menu />
+      </Toolbar>
+    </AppBar>
+    )
+  }
 }
-
-export default SiteBar
