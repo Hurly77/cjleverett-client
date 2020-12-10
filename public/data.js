c7cf54f7753e6bd5979c71fdfc5060d0c79f54f4
cameron <cameron.leverett@yahoo.com>
2020-12-09 22:23:21 -0800

    misc

---
+++ public/data.js
@@ -0,0 +1,23 @@
+const Data = 
+{
+  fontEnd: {
+    Title: "Front-end",
+    list: [
+      "React", 
+      "Redux",
+      "Thunk",
+      "Ajax",
+      "Material-UI",
+      "JavaSript",
+      "HTML", 
+      "CSS" 
+    ]
+  },
+  backend: {
+    Title: "Back-end",
+    list:[
+      "Ruby on Rails",
+      "REST Api"
+    ]
+  } 
+}
