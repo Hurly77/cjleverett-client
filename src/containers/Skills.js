15efa33dcf96d461d715c9f6ca38712d2fdc5115
cameron <cameron.leverett@yahoo.com>
2020-12-09 22:27:05 -0800

    added code to Skill component

---
+++ src/containers/Skills.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react'
+import Skill from '../components/skills/Skill'
+
+export default class Skills extends Component {
+  render() {
+    return (
+      <Skill />
+    )
+  }
+}
