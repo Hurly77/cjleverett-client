b90a40dfbd654fb3c5a2932d3b3a590f2f9e78c3
cameron <cameron.leverett@yahoo.com>
2020-12-09 22:25:54 -0800

    added some material

--- src/components/projects/Projects.js
+++ src/components/projects/Projects.js
@@ -1,11 +1,39 @@
-import React, { Component } from 'react'
+import React from 'react';
+import Paper from '@material-ui/core/Paper';
+import {Container, makeStyles} from '@material-ui/core';
 
-export default class Projects extends Component {
-  render() {
-    return (
-      <div>
-        
-      </div>
-    )
-  }
-}
+const useStyle = makeStyles((theme) => ({
+	root : {
+		display  : 'flex',
+    flexWrap : 'wrap',
+    justifyContent: 'center',
+    paddingLeft: '',
+    alignItems: 'center',
+    backgroundColor: '#20bf55',
+    paddingTop: '60px',
+		'& > *'  : {
+      backgroundColor: '#01baef',
+			margin : theme.spacing(3),
+			width  : theme.spacing(50),
+			height : theme.spacing(60),
+		},
+	},
+}));
+
+const Projects = (prop) => {
+  const classes = useStyle();
+  console.log(prop.prop)
+	return (
+    <>
+		<Container maxWidth="xl" className={classes.root} align="center">
+			<Paper elevation={20} />
+			<Paper elevation={20}/>
+      <Paper elevation={20} />
+      <Paper elevation={20} />
+      <Paper elevation={20} />
+		</Container>
+    </>
+	);
+};
+
+export default Projects;
