015e7c93f777cc12fb1cd11f66fd9941eb7fc55d
cameron <cameron.leverett@yahoo.com>
2020-12-09 22:27:16 -0800

    misc

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,12 +1,38 @@
 import React from 'react'
 import SiteBar from '../containers/SiteBar'
+import About from '../containers/About'
+import { Container, makeStyles, withStyles } from '@material-ui/core';
+import Skills from '../containers/Skills';
+import Projects from '../components/projects/Projects'
 
-const HomePage = () => {
+const useStyles = makeStyles({
+  root: {
+    zIndex: 1000,
+    borderRadius: '5px',
+    position: 'relative',
+    backgroundColor: '#fbfbff'
+  }
+})
+
+const styles = (theme) => ({
+  toolbar: theme.mixins.toolbar,
+});
+
+
+const HomePage = (props) => {
+  const obj = useStyles()
+  const { classes } = props;
+console.log(props)
   return (
     <div>
       <SiteBar />
+      <Container className={obj.root}>
+      <About prop={classes.toolbar} />
+      </Container>
+      <Skills />
+      <Projects prop={classes.toolbar}/>
     </div>
   )
 }
 
-export default HomePage
+export default withStyles(styles)(HomePage)
