44f8773536e4612f74ff0a7eba5a77b2733929d4
cameron <cameron.leverett@yahoo.com>
2020-12-09 22:26:14 -0800

    added componets from material ui lib

--- src/components/skills/Skill.js
+++ src/components/skills/Skill.js
@@ -1,11 +1,47 @@
-import React, { Component } from 'react'
+import {Container, makeStyles, Typography} from '@material-ui/core';
+import React from 'react';
+const useStyle = makeStyles({
+	root : {
+		backgroundColor : '#0b4f6c',
+    paddingTop      : '30px',
+    height: '300px',
+    '& h1': {
+      color           : '#01baef',
+      marginBottom: 0,
+      paddingBottom: 0,
+    },
+		'& section'         : {
+      transform: 'translate(0, 100px)',
+      border: 'solid 1px',
+			borderRadius    : '5px',
+			backgroundColor : '#fbfbff',
+			marginBottom    : '30px',
+			'& h1'          : {},
+			'& p'           : {
+				fontSize : '20px',
+			},
+		},
+	},
+});
 
-export default class Skills extends Component {
-  render() {
-    return (
-      <div>
-        
-      </div>
-    )
-  }
-}
+const Skill = () => {
+	const classes = useStyle();
+	return (
+		<Container align="center" className={classes.root} maxWidth="xl">
+			<Container component="section" maxWidth="sm">
+			<Typography component="h1" variant="h3">
+				Front-end:
+			</Typography>
+				<Typography display="inline" component="p">Html, Css, Material-UI, JavaScript, React, Redux, Thunk</Typography>
+
+      <Typography component="h1" variant="h3">
+					Back-end:
+				</Typography>
+				<Typography component="p">Material UI</Typography>
+			</Container>
+
+		</Container>
+	);
+};
+
+export default Skill;
