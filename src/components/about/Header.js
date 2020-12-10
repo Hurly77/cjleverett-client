690cc9b4d3f70c8490a1c8b56e934e67584bf365
cameron <cameron.leverett@yahoo.com>
2020-12-09 22:25:33 -0800

    added som style to header

--- src/components/about/Header.js
+++ src/components/about/Header.js
@@ -1,9 +1,15 @@
-import React from 'react'
+import React from 'react';
+import Typography from '@material-ui/core/Typography';
+import Container from '@material-ui/core/Container';
 
-export const Header = () => {
-  return (
-    <h1>
-     Lorem H1 
-    </h1>
-  )
-}
+export const Header = (prop) => {
+	return (
+		<Container maxWidth="sm" align={'center'} component="section">
+			<div className={prop.prop} />
+			<Typography component="h1" variant={'h2'}>Full Stack Developer</Typography>
+			<Typography component="p">I Build Full Stack application, I love what I do and, I always push myself to learn more evey day</Typography>
+		</Container>
+	);
+};
+
+export default Header;
