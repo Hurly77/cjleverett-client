0667df3481c2f8cddb6502b11cd86df1ba2aecec
cameron <cameron.leverett@yahoo.com>
2020-12-11 17:47:29 -0800

    added theme from material ui

--- src/index.js
+++ src/index.js
@@ -1,8 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import {createMuiTheme, ThemeProvider} from '@material-ui/core/styles'
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: '#01baef',
+      contrastText: '#fbfbff'
+    },
+    secondary: {main: '#0b4f6c'},
+    text: {
+      primary: '#fbfbff',
+      secondary: '#fbfbff'
+    }
+  }
+})
 
 ReactDOM.render(
-  <App />,
+  <ThemeProvider theme={theme}>
+    <App />,
+  </ThemeProvider>,
   document.getElementById('root')
 );
