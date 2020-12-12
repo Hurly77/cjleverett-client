ba76e450d9787e334b818a51e4a2a20114fd0483
cameron <cameron.leverett@yahoo.com>
2020-12-11 17:49:36 -0800

    more stylr

--- src/containers/SiteBar.js
+++ src/containers/SiteBar.js
@@ -1,20 +1,13 @@
 import React, { Component } from 'react'
 import AppBar from '@material-ui/core/AppBar'
 import Toolbar from '@material-ui/core/Toolbar'
-import {makeStyles} from '@material-ui/core/styles'
 import Logo from '../components/Logo'
 import Menu from '../components/Menu'
 
 export default class SiteBar extends Component {  
   render() {
-    const classes = makeStyles({
-      root: {
-        background: '#0b4f6c',
-      },
-    })
-
     return (
-      <AppBar position="fixed" className={classes.root}>
+      <AppBar position="fixed">
       <Toolbar>
         <Logo />
         <Menu />
