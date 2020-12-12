a454f0ea5a44226605bfdd782e912d7255828665
cameron <cameron.leverett@yahoo.com>
2020-12-11 17:49:01 -0800

    most of this is style change or add

--- src/components/skills/Skill.js
+++ src/components/skills/Skill.js
@@ -3,16 +3,16 @@ import React from 'react';
 const useStyle = makeStyles({
 	root : {
 		backgroundColor : '#0b4f6c',
-    paddingTop      : '30px',
-    height: '300px',
-    '& h1': {
-      color           : '#01baef',
-      marginBottom: 0,
-      paddingBottom: 0,
-    },
-		'& section'         : {
-      transform: 'translate(0, 100px)',
-      border: 'solid 1px',
+		paddingTop      : '30px',
+		height          : '300px',
+		'& h1'          : {
+			color         : '#01baef',
+			marginBottom  : 0,
+			paddingBottom : 0,
+		},
+		'& section'     : {
+			transform       : 'translate(0, -13px)',
+			border          : 'solid 1px',
 			borderRadius    : '5px',
 			backgroundColor : '#fbfbff',
 			marginBottom    : '30px',
@@ -21,25 +21,50 @@ const useStyle = makeStyles({
 				fontSize : '20px',
 			},
 		},
+		'& li' : {
+			listStyleType: 'none',
+			listStyle: 'none'
+		},
+		'& img' : {
+			backgroundColor: 'transparent',
+		}
 	},
 });
 
+
 const Skill = () => {
 	const classes = useStyle();
+	console.log(process.env.PUBLIC_URL + "Cameron Leverett.png")
 	return (
-		<Container align="center" className={classes.root} maxWidth="xl">
+		<Container align="center" className={classes.root} maxWidth="xl">	
+	
 			<Container component="section" maxWidth="sm">
-			<Typography component="h1" variant="h3">
-				Front-end:
-			</Typography>
-				<Typography display="inline" component="p">Html, Css, Material-UI, JavaScript, React, Redux, Thunk</Typography>
+				<Typography component="h1" variant="h3">
+					Front-end:
+				</Typography>
+				<Typography display="inline" component="p">
+						<li>HTML,</li>
+						<li>CSS,</li>
+						<li>Material,</li>
+						<li>JavaScript,</li>
+						<li>React,</li>
+						<li>Redux,</li>
+						<li>Thunk,</li>
+				</Typography>
 
-      <Typography component="h1" variant="h3">
+				<Typography component="h1" variant="h3">
 					Back-end:
 				</Typography>
-				<Typography component="p">Material UI</Typography>
+				<Typography component="p" >
+					<li>Ruby on Rails,</li>
+					<li>Rest,</li>
+					<li>Validation<br />Authentication,</li>
+					<li>APIs,</li>
+					<li>SQL,</li>
+					<li>MVC,</li>
+					<li>AREL,</li>
+				</Typography>
 			</Container>
-
 		</Container>
 	);
 };
