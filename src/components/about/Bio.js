f0e819072722ff2db080db2b6d0f5bfc796360d1
cameron <cameron.leverett@yahoo.com>
2021-01-26 17:39:05 -0800

    misc

--- src/components/about/Bio.js
+++ src/components/about/Bio.js
@@ -1,13 +1,20 @@
-import { Container, Typography } from '@material-ui/core'
+import { Container, Typography, makeStyles} from '@material-ui/core'
 import React from 'react'
 
+const useStyles = makeStyles((theme) => ({
+  root: {
+    color: theme.primary
+  }
+}))
+
 const Bio = () => {
+  const classes = useStyles()
   return (
-   <Container component="section" align={'center'}>
-     <Typography component="h2" variant={'h5'}>
+   <Container className={classes.root} component="section" align={'center'}>
+     <Typography className={classes.root} component="h2" variant={'h5'}>
         Hi, I'm Cameron
      </Typography>
-     <Typography component="p">
+     <Typography className={classes.root} component="p">
      I love Computers, whether I'm building one, developing web applications, or just reading about NVIDIA's new Ampere architecture. Whatever the case may be. The point is I love what I do.
      </Typography>
    </Container>
