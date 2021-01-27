ace32ca92b88e620fd5f16d4df2e3b4472f4bce3
cameron <cameron.leverett@yahoo.com>
2021-01-26 17:40:09 -0800

    added style to skill comp

--- src/components/skills/Skill.js
+++ src/components/skills/Skill.js
@@ -2,61 +2,62 @@ import {Container, makeStyles, Typography} from '@material-ui/core';
 import React from 'react';
 const useStyle = makeStyles({
 	root : {
-		backgroundColor : '#0b4f6c',
+		backgroundColor : '#01baef',
 		paddingTop      : '30px',
 		height          : '300px',
 		'& h1'          : {
-			color         : '#01baef',
+			color         : '#20bf55',
 			marginBottom  : 0,
 			paddingBottom : 0,
 		},
 		'& section'     : {
-			transform       : 'translate(0, -13px)',
-			border          : 'solid 1px',
-			borderRadius    : '5px',
-			backgroundColor : '#fbfbff',
+			transform       : 'translate(0, 40px)',
+			color           : '#fbfbfb',
+			border          : 'solid 3px #01baef',
+			borderRadius    : '10px',
+			backgroundColor : '#0b4f6c',
 			marginBottom    : '30px',
 			'& h1'          : {},
 			'& p'           : {
 				fontSize : '20px',
 			},
 		},
-		'& li' : {
-			listStyleType: 'none',
-			listStyle: 'none'
+		'& li'          : {
+			listStyleType : 'none',
+			listStyle     : 'none',
 		},
-		'& img' : {
-			backgroundColor: 'transparent',
-		}
+		'& img'         : {
+			backgroundColor : 'transparent',
+		},
 	},
 });
 
-
 const Skill = () => {
 	const classes = useStyle();
-	console.log(process.env.PUBLIC_URL + "Cameron Leverett.png")
+	console.log(process.env.PUBLIC_URL + 'Cameron Leverett.png');
 	return (
-		<Container align="center" className={classes.root} maxWidth="xl">	
-	
+		<Container align="center" className={classes.root} maxWidth="xl">
 			<Container component="section" maxWidth="sm">
 				<Typography component="h1" variant="h3">
 					Front-end:
 				</Typography>
 				<Typography display="inline" component="p">
-						<li>Material-UI,</li>
-						<li>JavaScript,</li>
-						<li>React,</li>
-						<li>Redux,</li>
-						<li>Thunk,</li>
+					<li>Material-UI,</li>
+					<li>JavaScript,</li>
+					<li>React,</li>
+					<li>Redux,</li>
+					<li>Thunk,</li>
 				</Typography>
 
 				<Typography component="h1" variant="h3">
 					Back-end:
 				</Typography>
-				<Typography component="p" >
+				<Typography component="p">
 					<li>Ruby on Rails,</li>
 					<li>Rest,</li>
-					<li>Validation,<br />Authentication,</li>
+					<li>
+						Validation,<br />Authentication,
+					</li>
 					<li>APIs,</li>
 					<li>SQL,</li>
 				</Typography>
